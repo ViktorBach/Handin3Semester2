e7a1638e10af33f87ce1c246dee8691b97b6fc56
ViktorBach <vmb0107@gmail.com>
2024-02-22 14:24:02 +0100

    created reddit objects and solved exercises

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -3,6 +3,8 @@ import `2`.FastFood
 import `2`.Pizza
 import `2`.Sandwich
 import `3`.Mazda
+import `4`.RedditFrontPage
+import `4`.RedditPost
 
 fun main () {
     // 1. Articles
@@ -32,4 +34,23 @@ fun main () {
     println(mx5.changeGear(30))
     println(mx5.applyBrakes(30))
     println(mx5.speedUp(30))
+
+    // 4. Reddit
+    val redditPost1: RedditPost = RedditPost("Ben Shapiro", "Hello World")
+    val redditPost2: RedditPost = RedditPost("Thomas Larsen", "Hello World")
+    val redditPost3: RedditPost = RedditPost("Frank Hvam", "Hello World")
+    val redditPost4: RedditPost = RedditPost("Jens Ferdinand", "Hello World")
+    val redditPost5: RedditPost = RedditPost("Elon Musk", "Hello World")
+    val listOfRedditPosts: List<RedditPost> = listOf(redditPost1, redditPost2, redditPost3, redditPost4, redditPost5)
+    redditPost1.downvotes = -200
+    redditPost2.upvotes = 20
+    redditPost3.downvotes = -100
+    redditPost4.upvotes = 50
+    redditPost5.upvotes = 100
+    println(listOfRedditPosts.sortedByDescending { it.upvotes })
+    println(redditPost1.downvotes)
+
+    // 4.5 Reddit Front Page
+    val redditFrontPage: RedditFrontPage = RedditFrontPage(mutableListOf(redditPost1, redditPost2, redditPost3, redditPost4, redditPost5))
+    println(redditFrontPage.deleteRedditPost())
 }
